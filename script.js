e40b20b3b8018efdf132b0077b893f294344c3bf
jarguello1 <jcarguello11@live.com>
2022-08-29 11:48:52 -0400

    add class constructor and examples

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 let myLibrary = [];
 
 function Book(title, author, pages, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readStatus = readStatus;
+    constructor(
+        title = 'Unknown',
+        author = 'Unknown',
+        pages = '0',
+        isRead = false
+    ) 
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+    
 }
 
-function addBookToLibrary() {
-    //do stuff here
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+}
+
+
+const animalFarm = new Book('Animal Farm', 'George Orwell', 112, 'not read');
+const eotw = new Book('Eye of the World', 'Robert Jordan', 782, 'not read');
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 310, 'not read');
+
+addBookToLibrary(animalFarm);
+addBookToLibrary(eotw);
+addBookToLibrary(theHobbit)
+
+
+console.log(myLibrary);
+
+const container = document.querySelector('#container');
+
+const bookCard = document.createElement('div');
+bookCard.classList.add('bookCard');
+
+for (books in myLibrary) {
+
 }

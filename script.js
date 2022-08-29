d5603cc65e7c2169cb3835af9eca2b8eb4dc2004
jarguello1 <jcarguello11@live.com>
2022-08-28 22:48:47 -0400

    create myLibrary array and add book function

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+let myLibrary = [];
+
 function Book(title, author, pages, readStatus) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.readStatus = readStatus;
+}
+
+function addBookToLibrary() {
+    //do stuff here
 }

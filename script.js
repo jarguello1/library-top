68bafc2167855bac49639a25ae8f995af6c9c563
jarguello1 <jcarguello11@live.com>
2022-08-29 12:34:02 -0400

    create function to make book cards

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 let myLibrary = [];
 
 function Book(title, author, pages, readStatus) {
-    constructor(
-        title = 'Unknown',
-        author = 'Unknown',
-        pages = '0',
-        isRead = false
-    ) 
         this.title = title;
         this.author = author;
         this.pages = pages;
@@ -19,9 +13,9 @@ function addBookToLibrary(book) {
 }
 
 
-const animalFarm = new Book('Animal Farm', 'George Orwell', 112, 'not read');
-const eotw = new Book('Eye of the World', 'Robert Jordan', 782, 'not read');
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 310, 'not read');
+const animalFarm = new Book('Animal Farm', 'George Orwell', 112, false);
+const eotw = new Book('Eye of the World', 'Robert Jordan', 782, false);
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 310, false);
 
 addBookToLibrary(animalFarm);
 addBookToLibrary(eotw);
@@ -32,9 +26,52 @@ console.log(myLibrary);
 
 const container = document.querySelector('#container');
 
-const bookCard = document.createElement('div');
-bookCard.classList.add('bookCard');
+function createBookCard(book) {
+    const bookCard = document.createElement('div');
+    const title = document.createElement('p');
+    const author = document.createElement('p')
+    const pages = document.createElement('p');
+    const readStatus = document.createElement('button');
+    const removeBook = document.createElement('button');
+    const infoContainer = document.createElement('div');
+    const buttonContainer = document.createElement('div');
+
+    bookCard.classList.add('bookCard');
+    infoContainer.classList.add('bookInfo');
+    buttonContainer.classList.add('btnContainer');
+    readStatus.classList.add('btn');
+    removeBook.classList.add('btn');
+
+    title.textContent = book.title;
+    title.classList.add('Title');
+    infoContainer.appendChild(title);
+
+    author.textContent = book.author;
+    author.classList.add('Author');
+    infoContainer.appendChild(author);
+
+    pages.textContent = book.pages;
+    pages.classList.add('Pages');
+    infoContainer.appendChild(pages);
+
+    if (book.isRead) {
+        readStatus.textContent = 'Read'
+        readStatus.style.backgroundColor = '#63da63'
+      } else {
+        readStatus.textContent = 'Not read'
+        readStatus.style.backgroundColor = '#e04f63';
+      }
+    buttonContainer.appendChild(readStatus);
+
+    removeBook.textContent = "Remove Book";
+    buttonContainer.appendChild(removeBook);
+
+    bookCard.appendChild(infoContainer);
+    bookCard.appendChild(buttonContainer);
+
+    container.appendChild(bookCard);
+}
 
 for (books in myLibrary) {
-
+    createBookCard(Book);
 }
